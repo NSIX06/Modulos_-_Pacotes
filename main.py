879235e0dda3326c248981811e0c56435884a304
NSIX06 <felipebugalho2016@gmail.com>
2024-10-04 19:17:25 -0400

    100%

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from matematica import funcao_soma, funcao_divisao, funcao_subtracao, funcao_multiplicacao
-
+from matematica import *
 
 print(funcao_soma(10, 2))
 print(funcao_divisao(10, 2))
 print(funcao_subtracao(10, 2))
 print(funcao_multiplicacao(10, 2))
 
-from matematica.avancados import quadrado, cubo
+from matematica.avancados import *
 print(quadrado.funcao_quadrado(10))
 print(cubo.funcao_cubo(10))
 
